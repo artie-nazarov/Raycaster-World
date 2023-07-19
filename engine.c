acf0044534ece990ebdbc8b4463a08134c6a9649
Artie Nazarov <artienazarov@gmail.com>
2023-07-19 12:41:48 -0700

    added missing horizontal line code. improved precision at the horizontal edges for player angle

--- engine.c
+++ engine.c
@@ -17,37 +17,122 @@
 /// Map Properties
 #define MAP_FILEPATH "maps/map1.map"
 #define WALL_BLOCK_SIZE 64
+#define WALL_BLOCK_POWER 6
 
 /// Other Properties
 #define PI 3.1415926535
 #define RAY_LEN_FACTOR 10
+#define EPS 1e-5
 
-float px, py;       // player position global variables
+int px, py;         // player position global variables
 float pa;           // player position angle in randians
 float x_off, y_off; // x and y offsets based on player angle
+Map *m;             // Map object
+
+/// Raycasting
+void raycast() {
+    float ra;               // ray angle
+    int hdx, hdy, hrx, hry; // horizontal ray intercept points and offests
+    int vdx, vdy, vrx, vry; // vertical ray intercept points and offests
+    int dop;                // depth of perception value in terms of number of blocks (how far a player can see)
+    // set intial ray angle to player's view angle
+    ra = pa;
+    // define depth of perception max ray length
+    dop = 10;
+
+    /// Based on the direction of a ray compute the first Horizonatal and Vertical line intercepts
+    /// Horizontal intercept
+    float recip_tan = -(1 / tan(ra));  // reciprocal of tan(ra)
+    // ray facing sideways (not up or down)
+    bool ra_iszero = ((ra <= EPS && ra >= 0) || (ra >= -EPS && ra <= 0));
+    if ( ra_iszero || ((ra >= M_PI - EPS && ra <= M_PI + EPS))) {
+        hrx = px;
+        hry = py;
+    }
+    // ray facing upward
+    else if (ra > M_PI) {
+        // y-intercept
+        // The nearest upper block edge y coordinate
+        hry = ((py >> WALL_BLOCK_POWER) << WALL_BLOCK_POWER) - 0.00001;
+        // x-offset
+        hrx = (py - hry) * recip_tan + px;
+        hdy = -WALL_BLOCK_SIZE;
+        hdx = -hdy * recip_tan;
+    }
+    // ray facing downward
+    else if (ra < M_PI) {
+        // y-intercept
+        // The nearest lower block edge y coordinate
+        hry = ((py + 64) >> WALL_BLOCK_POWER) << WALL_BLOCK_POWER;
+        // x-offset
+        hrx = (py - hry) * recip_tan + px;
+        hdy = WALL_BLOCK_SIZE;
+        hdx = -hdy * recip_tan;
+    }
+    
+    // Cast a ray until it hits a wall
+    int ix, iy;
+    while (dop) {
+        // check horizontal wall
+        ix = hrx >> WALL_BLOCK_POWER;
+        iy = hry >> WALL_BLOCK_POWER;
+        // Check map boundaries
+        if ((ix >= 0 && ix < m->width) && (iy > 0 && iy < m->height)) {
+            // we hit a wall
+            //printf("(%d, %d) -> (%d, %d) -> %c\n",hry, hrx, iy, ix, m->map[getIndex(m, iy, ix)]);
+            if (m->map[getIndex(m, iy, ix)] == '1') {dop = 1;}
+            // otherwise keep casting
+            else {
+                hrx += hdx;
+                hry += hdy;
+            }
+        }
+
+        dop--;
+    }
+
+    // Select the smaller distance to a wall (horizontal vs vertical)
+    bool vertical = false;
+    // float v_distance = sqrt((px - vrx) * (px - vrx) + (py - vry) * (py - vry));
+    // float h_distance = sqrt((px - hrx) * (px - hrx) + (py - hry) * (py - hry));
+    //vertical = (v_distance > h_distance);
+    int rx, ry;
+    if (vertical) {
+        rx = vrx;
+        ry = vry;
+    }
+    else {
+        rx = hrx;
+        ry = hry;
+    }
+    //printf("player_pos: (%d, %d) ray_pos: (%d, %d)  delta_x: (%d)\n", px, py, rx, ry, hdx);
+    // Draw resulting ray
+    glColor3f(0, 1, 0);
+    glLineWidth(1);
+    glBegin(GL_LINES);
+        glVertex2i(px, py);
+        glVertex2i(rx, ry);
+    glEnd();
+
+}
 
 /// Draw a player point
 void drawPlayer() {
     glColor3f(1, 1, 0);     // yellow color
     glPointSize(10);        // set the point size
     glBegin(GL_POINTS);     // specify geometricx object as POINTS (multiple objects)
-        glVertex2f(px, py); // define a single point at the player coordinates
+        glVertex2i(px, py); // define a single point at the player coordinates
     glEnd();
     // Draw player "vision" vector
     glLineWidth(3);
     glBegin(GL_LINES);
-        glVertex2f(px, py);
-        glVertex2f(px+x_off*RAY_LEN_FACTOR, py+y_off*RAY_LEN_FACTOR);
+        glVertex2i(px, py);
+        glVertex2i(px+x_off*RAY_LEN_FACTOR, py+y_off*RAY_LEN_FACTOR);
     glEnd();
 }
 
 /// Draw Map
 void drawMap2D() {
-    // Create a new Map object
-    Map *m = initMap(MAP_FILEPATH);
-    if (m == NULL) {
-        return;
-    }
     // Iterate over the map
     // If a wall is seen, draw a cube with dim3(WALL_BLOCK_SIZE,..,..)
     int w = m->width, h = m->height;
@@ -77,7 +162,7 @@ void drawMap2D() {
 void playerMove(unsigned char key, int x, int y) {
     switch (key) {
         case 'w': px += x_off; py += y_off; break;
-        case 's': px += x_off; py += y_off; break;
+        case 's': px -= x_off; py -= y_off; break;
         case 'a': pa -= 0.1; 
                   pa = (pa < 0) ? (2 * M_PI + pa) : pa;
                   x_off = cos(pa) * STEP_SIZE;
@@ -90,6 +175,8 @@ void playerMove(unsigned char key, int x, int y) {
                   break;
         default: break;
     }
+    printf("rad=%f cos(t)=%f sin(t)=%f tan(t)=%f\n", pa, cos(pa), sin(pa), tan(pa));
+
     glutPostRedisplay();
 }
 
@@ -98,6 +185,7 @@ void display() {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);  // Clear the color buffer (background)
     drawMap2D();
     drawPlayer();
+    raycast();
     glutSwapBuffers();
 }
 
@@ -105,10 +193,15 @@ void display() {
 void init() {
     glClearColor(0.3, 0.3, 0.3, 0);  // Set background color to grey
     gluOrtho2D(0, WIDTH, HEIGHT, 0); // Define the window size
-    px = 300, py = 300;
-    pa = 0;
-    x_off = cos(pa) * STEP_SIZE;
-    y_off = sin(pa) * STEP_SIZE;
+    // Create a new Map object
+    m = initMap(MAP_FILEPATH);
+    if (m == NULL) {
+        return;
+    }
+    px = 300, py = 300;              // player initial coorindates
+    pa = 0.1;                        // player initial view angle
+    x_off = cos(pa) * STEP_SIZE;     // player view x_offset
+    y_off = sin(pa) * STEP_SIZE;     // player view y_offset
 }
 
 /// Main
