c787be4ae7e82aed813c42615d7cea00f9f90e52
Artie Nazarov <artienazarov@gmail.com>
2023-07-19 17:02:44 -0700

    multi-ray casting

--- engine.c
+++ engine.c
@@ -31,146 +31,150 @@ Map *m;             // Map object
 
 /// Raycasting
 void raycast() {
-    float ra;               // ray angle
+    float ra = pa - M_PI_4; // ray angle
+    float right_angle = ra; // used to traverse a circle 90 degrees
     int hdx, hdy, hrx, hry; // horizontal ray intercept points and offests
     int vdx, vdy, vrx, vry; // vertical ray intercept points and offests
     int dop;                // depth of perception value in terms of number of blocks (how far a player can see)
-    // set intial ray angle to player's view angle
-    ra = pa;
-    // define depth of perception max ray length
-    dop = 10;
 
-    /// Based on the direction of a ray compute the first Horizonatal and Vertical line intercepts
-    /// Vertical intercept
-    float tanra = tan(ra);
-    // ray facing right
-    if (ra < M_PI_2 || ra > 3*M_PI_2) {
-        // x-intercept
-        // The nearest right block edge x coordinate
-        vrx = ((px + 64) >> WALL_BLOCK_POWER) << WALL_BLOCK_POWER;
-        // y-offset
-        vry = (vrx-px) * tanra + py;
-        // delta offsets
-        vdx = WALL_BLOCK_SIZE;
-        vdy = vdx * tanra;
-    }
-    // ray facing left
-    else if (ra > M_PI_2 && ra < 3*M_PI_2) {
-        // tangent value should be flipped for 3 and 4 quadrants
-        tanra *= -1;
-        // x-intercept
-        // The nearest left block edge x coordinate
-        vrx = ((px >> WALL_BLOCK_POWER) << WALL_BLOCK_POWER) - 0.0001;
-        // y-offset
-        vry = (px-vrx) * tanra + py;
-        // delta offsets
-        vdx = -WALL_BLOCK_SIZE;
-        vdy = -vdx * tanra;
-    }
-    // ray facing upward or downward
-    else {
-        vrx = px;
-        vry = py;
-        dop = 0;
-    }
-    /// Horizontal intercept
-    float recip_tan = -(1 / tan(ra));  // reciprocal of tan(ra)
-    // ray facing sideways (not up or down)
-    bool ra_iszero = ((ra <= EPS && ra >= 0) || (ra >= -EPS && ra <= 0));
-    if ( ra_iszero || ((ra >= M_PI - EPS && ra <= M_PI + EPS))) {
-        hrx = px;
-        hry = py;
-        hdx = WALL_BLOCK_SIZE;
-        hdy=0;
-    }
-    // ray facing upward
-    else if (ra > M_PI) {
-        // y-intercept
-        // The nearest upper block edge y coordinate
-        hry = ((py >> WALL_BLOCK_POWER) << WALL_BLOCK_POWER) - 0.00001;
-        // x-offset
-        hrx = (py - hry) * recip_tan + px;
-        hdy = -WALL_BLOCK_SIZE;
-        hdx = -hdy * recip_tan;
-    }
-    // ray facing downward
-    else if (ra < M_PI) {
-        // y-intercept
-        // The nearest lower block edge y coordinate
-        hry = ((py + 64) >> WALL_BLOCK_POWER) << WALL_BLOCK_POWER;
-        // x-offset
-        hrx = (py - hry) * recip_tan + px;
-        hdy = WALL_BLOCK_SIZE;
-        hdx = -hdy * recip_tan;
-    }
-    
-    // Cast a ray until it hits a wall
-    int ix, iy;                  // indices into the map
-    bool vertical_hit = false;   // check whether we hit a vertical edge
-    bool horizontal_hit = false; // check whether we hit a horizontal edge
-    while (dop) {
-        // check horizontal wall
-        ix = hrx >> WALL_BLOCK_POWER;
-        iy = hry >> WALL_BLOCK_POWER;
-        // Check map boundaries and whether we already found a horizontal hit
-        if ((ix >= 0 && ix < m->width) && (iy >= 0 && iy < m->height) && !horizontal_hit) {
-            // we hit a wall
-            //printf("HORIZONTAL (%d, %d) -> (%d, %d) -> %c\n", hrx, hry, iy, ix, m->map[getIndex(m, iy, ix)]);
-            if (m->map[getIndex(m, iy, ix)] == '1') { horizontal_hit = true;}
-            // otherwise keep casting
-            else {
-                hrx += hdx;
-                hry += hdy;
-            }
-        }
-        // check vertical wall
-        ix = vrx >> WALL_BLOCK_POWER;
-        iy = vry >> WALL_BLOCK_POWER;
-        // Check map boundaries and whether we already found a vertical hit
-        if ((ix >= 0 && ix < m->width) && (iy >= 0 && iy < m->height) && !vertical_hit) {
-            //printf("VERTICAL (%d, %d) -> (%d, %d) -> %c\n", vrx, vry, iy, ix, m->map[getIndex(m, iy, ix)]);
-            if (m->map[getIndex(m, iy, ix)] == '1') { vertical_hit = true;}
-            // otherwise keep casting
-            else {
-                vrx += vdx;
-                vry += vdy;
-            }
-        }
-        dop--;
-    }
+    while (right_angle < pa + M_PI_4) { 
+        // check ray angle bounds
+        ra = (ra < 0) ? (2 * M_PI + ra) : ra;
+        ra = (ra >= 2 * M_PI) ? (ra - 2 * M_PI) : ra;
+        // define depth of perception max ray length
+        dop = 10;
 
-    // Select the smaller distance to a wall (horizontal vs vertical)
-    int rx, ry; // ray end point coordinates
-    // if ray hit both edges, pick one with the shortest distance from player
-    if (horizontal_hit && vertical_hit) {
-        //printf("BOTH\n");
-        float v_distance = sqrt((px - vrx) * (px - vrx) + (py - vry) * (py - vry));
-        float h_distance = sqrt((px - hrx) * (px - hrx) + (py - hry) * (py - hry));
-        bool vertical = (v_distance < h_distance);
-        rx = vertical ? vrx : hrx;
-        ry = vertical ? vry : hry;
+        /// Based on the direction of a ray compute the first Horizonatal and Vertical line intercepts
+        /// Vertical intercept
+        float tanra = tan(ra);
+        // ray facing right
+        if (ra < M_PI_2 || ra > 3*M_PI_2) {
+            // x-intercept
+            // The nearest right block edge x coordinate
+            vrx = ((px + 64) >> WALL_BLOCK_POWER) << WALL_BLOCK_POWER;
+            // y-offset
+            vry = (vrx-px) * tanra + py;
+            // delta offsets
+            vdx = WALL_BLOCK_SIZE;
+            vdy = vdx * tanra;
+        }
+        // ray facing left
+        else if (ra > M_PI_2 && ra < 3*M_PI_2) {
+            // tangent value should be flipped for 3 and 4 quadrants
+            tanra *= -1;
+            // x-intercept
+            // The nearest left block edge x coordinate
+            vrx = ((px >> WALL_BLOCK_POWER) << WALL_BLOCK_POWER) - 0.0001;
+            // y-offset
+            vry = (px-vrx) * tanra + py;
+            // delta offsets
+            vdx = -WALL_BLOCK_SIZE;
+            vdy = -vdx * tanra;
+        }
+        // ray facing upward or downward
+        else {
+            vrx = px;
+            vry = py;
+            dop = 0;
+        }
+        /// Horizontal intercept
+        float recip_tan = -(1 / tan(ra));  // reciprocal of tan(ra)
+        // ray facing sideways (not up or down)
+        bool ra_iszero = ((ra <= EPS && ra >= 0) || (ra >= -EPS && ra <= 0));
+        if ( ra_iszero || ((ra >= M_PI - EPS && ra <= M_PI + EPS))) {
+            hrx = px;
+            hry = py;
+            hdx = WALL_BLOCK_SIZE;
+            hdy=0;
+        }
+        // ray facing upward
+        else if (ra > M_PI) {
+            // y-intercept
+            // The nearest upper block edge y coordinate
+            hry = ((py >> WALL_BLOCK_POWER) << WALL_BLOCK_POWER) - 0.00001;
+            // x-offset
+            hrx = (py - hry) * recip_tan + px;
+            hdy = -WALL_BLOCK_SIZE;
+            hdx = -hdy * recip_tan;
+        }
+        // ray facing downward
+        else if (ra < M_PI) {
+            // y-intercept
+            // The nearest lower block edge y coordinate
+            hry = ((py + 64) >> WALL_BLOCK_POWER) << WALL_BLOCK_POWER;
+            // x-offset
+            hrx = (py - hry) * recip_tan + px;
+            hdy = WALL_BLOCK_SIZE;
+            hdx = -hdy * recip_tan;
+        }
+        
+        // Cast a ray until it hits a wall
+        int ix, iy;                  // indices into the map
+        bool vertical_hit = false;   // check whether we hit a vertical edge
+        bool horizontal_hit = false; // check whether we hit a horizontal edge
+        while (dop) {
+            // check horizontal wall
+            ix = hrx >> WALL_BLOCK_POWER;
+            iy = hry >> WALL_BLOCK_POWER;
+            // Check map boundaries and whether we already found a horizontal hit
+            if ((ix >= 0 && ix < m->width) && (iy >= 0 && iy < m->height) && !horizontal_hit) {
+                // we hit a horizontal wall
+                if (m->map[getIndex(m, iy, ix)] == '1') { horizontal_hit = true;}
+                // otherwise keep casting
+                else {
+                    hrx += hdx;
+                    hry += hdy;
+                }
+            }
+            // check vertical wall
+            ix = vrx >> WALL_BLOCK_POWER;
+            iy = vry >> WALL_BLOCK_POWER;
+            // Check map boundaries and whether we already found a vertical hit
+            if ((ix >= 0 && ix < m->width) && (iy >= 0 && iy < m->height) && !vertical_hit) {
+                // we hit a vertical wall
+                if (m->map[getIndex(m, iy, ix)] == '1') { vertical_hit = true;}
+                // otherwise keep casting
+                else {
+                    vrx += vdx;
+                    vry += vdy;
+                }
+            }
+            dop--;
+        }
+
+        // Select the smaller distance to a wall (horizontal vs vertical)
+        int rx, ry; // ray end point coordinates
+        // if ray hit both edges, pick one with the shortest distance from player
+        if (horizontal_hit && vertical_hit) {
+            float v_distance = sqrt((px - vrx) * (px - vrx) + (py - vry) * (py - vry));
+            float h_distance = sqrt((px - hrx) * (px - hrx) + (py - hry) * (py - hry));
+            bool vertical = (v_distance < h_distance);
+            rx = vertical ? vrx : hrx;
+            ry = vertical ? vry : hry;
+        }
+        // Horizontal hit occurred
+        else if (horizontal_hit) {
+            rx = hrx;
+            ry = hry;
+        }
+        // vertical hit occurred
+        else {
+            rx = vrx;
+            ry = vry;
+        }
+        
+        // Draw resulting ray
+        glColor3f(0, 1, 0);
+        glLineWidth(1);
+        glBegin(GL_LINES);
+            glVertex2i(px, py);
+            glVertex2i(rx, ry);
+        glEnd();
+
+        // update angle values
+        ra += 0.05;         // next ray angle (unit circle bounds unchecked)
+        right_angle += 0.05; // update the loop counter
     }
-    // Horizontal hit occurred
-    else if (horizontal_hit) {
-        //printf("HOR\n");
-        rx = hrx;
-        ry = hry;
-    }
-    // vertical hit occurred
-    else {
-        //printf("VERT\n");
-        rx = vrx;
-        ry = vry;
-    }
-    
-    //printf("player_pos: (%d, %d) ray_pos: (%d, %d)\n", px, py, rx, ry);
-    // Draw resulting ray
-    glColor3f(0, 1, 0);
-    glLineWidth(1);
-    glBegin(GL_LINES);
-        glVertex2i(px, py);
-        glVertex2i(rx, ry);
-    glEnd();
 
 }
 
@@ -257,7 +261,7 @@ void init() {
         return;
     }
     px = 350, py = 300;              // player initial coorindates
-    pa = 0.1;                        // player initial view angle
+    pa = M_PI_2;                        // player initial view angle
     x_off = cos(pa) * STEP_SIZE;     // player view x_offset
     y_off = sin(pa) * STEP_SIZE;     // player view y_offset
 }
